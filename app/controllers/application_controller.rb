5c9d3a6f3f6f295fedae69819312bce03699e9c5
Łukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-07-15 10:30:39 +0200

    Update gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    render html: "¡Hell world!"
+  end
+
+  def goodbye
+    render html: "Goodbye<% console %>"
+  end
+
+  def test
+    render html: "#{params[:id]}"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get '/', to:  'application#hello'
+  get '/test/:id', to: 'application#test'
+  get :goodbye, action: :goodbye, controller: 'application'
+
 end
