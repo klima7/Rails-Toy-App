19cc383018562b1217513ff2fde3434726ec0a27
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-07-19 11:04:43 +0200

    Refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  resources :microposts
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   get '/', to:  'application#hello'
   get '/test/:id', to: 'application#test'
   get :goodbye, action: :goodbye, controller: 'application'
 
+  resources :microposts
+  resources :users
 end
