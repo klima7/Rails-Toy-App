b42ec4d198bc2995292769d5ea2f315757687e95
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-07-20 11:30:22 +0200

    Add implementation for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to:  'application#hello'
   get 'static_pages/home'
   get 'static_pages/help'
-  get '/', to:  'application#hello'
+  get 'static_pages/about'
   get '/test/:id', to: 'application#test'
   get :goodbye, action: :goodbye, controller: 'application'
 
