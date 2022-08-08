699204bfc8e027be42e4760dfd37bc9df6abc444
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-08-08 12:35:08 +0200

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_17_110821) do
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_08_102450) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
