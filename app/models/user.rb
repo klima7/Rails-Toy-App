c2b92353eb4c2302928793f920fe5fef3b552c4e
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-08-08 18:12:21 +0200

    Run annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
